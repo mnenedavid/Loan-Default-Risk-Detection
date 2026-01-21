cf6715c813be7973c2c61b980c709278613e4c0f
mnenedavid <mnenedavid@gmail.com>
2026-01-21 23:27:11 +0300

    Add import for os module

--- Deployed_app/LOAN_DEFAULT_RISK_APP.py
+++ Deployed_app/LOAN_DEFAULT_RISK_APP.py
@@ -7,6 +7,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import joblib
+import os
 import plotly.graph_objects as go
 from sklearn.base import BaseEstimator, TransformerMixin
 
